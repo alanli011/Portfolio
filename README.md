1913b0ee1bb18aba8db9f8e5edd7989539394966
Alan Li <li.alan011@gmail.com>
2018-03-15 23:13:38 -0700

    First Commit. Made changes to Portfolio

---
+++ README.md
@@ -0,0 +1,2 @@
+# Portfolio
+Hi, my name is Alan and this is my portfolio. Please come and check it out!
---
+++ js/mixitup-config.js
@@ -0,0 +1,7 @@
+// Mixitup Settings
+var containerEl = document.querySelector('#portfolioList');
+var mixer = mixitup(containerEl, {
+  selectors: {
+    control: '[data-mixitup-control]'
+  }
+});
